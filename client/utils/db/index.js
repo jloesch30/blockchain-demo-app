e82fdd46e7b18be9702880593b529d189e8d675d
Josh Loesch <jloesch30@gmail.com>
2022-04-07 16:04:21 -0500

    cert for firebase change

--- client/utils/db/index.js
+++ client/utils/db/index.js
@@ -4,7 +4,11 @@ import serviceAccount from "./serviceAccountKey.json";
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert({
+        clientEmail: process.env.CLIENT_EMAIL,
+        privateKey: process.env.PRIVATE_KEY,
+        projectId: process.env.PROJECT_ID,
+      }),
     });
   } catch (error) {
     console.log("Firebase admin initialization error", error.stack);
