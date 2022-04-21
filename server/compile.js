852578626150c25a98f55f54c91e7e025ada9bdb
rskim47 <rickkim95@gmail.com>
2022-04-21 18:21:26 -0500

    adding comments to the scripts

--- server/compile.js
+++ server/compile.js
@@ -1,8 +1,12 @@
+// Compiles the sol file and exports 
+
 const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
 
+// Accessing the file on harddrive
 const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
 const source = fs.readFileSync(lotteryPath, "utf8");
 
-module.exports = solc.compile(source, 1).contracts[":Lottery"];
+// Exporting Inbox Contract
+module.exports = solc.compile(source, 1).contracts[ ":Lottery" ];
