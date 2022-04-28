e9e4db506a19c44242444b0768571e9e25aa92db
Josh Loesch <jloesch30@gmail.com>
2022-04-27 21:42:22 -0500

    whitelist work

---
+++ client/context/user.js
@@ -0,0 +1,23 @@
+import { TenantAwareAuth } from "firebase-admin/lib/auth/tenant-manager";
+import { createContext, useContext, useEffect } from "react";
+import { useEffect } from "react";
+
+const AppContext = createContext();
+
+export function AppWrapper({ children }) {
+  let sharedState = {};
+
+  useEffect(() => {
+    const getWhiteList = async () => {
+      const check = await 
+    }
+  }, [sharedState.admin]);
+
+  return (
+    <AppContext.Provider value={sharedState}>{children}</AppContext.Provider>
+  );
+}
+
+export function useAppContext() {
+  return useContext(AppWrapper);
+}
--- client/pages/api/admin/whitelist.js
+++ client/pages/api/admin/whitelist.js
@@ -1,7 +1,9 @@
 import { getSession } from "next-auth/react";
+import { resolveConfig } from "prettier";
 
 export default async function handler(req, res) {
   const session = await getSession({ req });
   if (session) {
+    console.log("Getting the whitelisted users");
   }
 }
