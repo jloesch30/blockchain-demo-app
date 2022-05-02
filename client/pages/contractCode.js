e682c382bab820e8fe68e4f8057838f3c6782a60
Josh Loesch <jloesch30@gmail.com>
2022-05-02 17:08:43 -0500

    contract code place holder

---
+++ client/pages/contractCode.js
@@ -0,0 +1,12 @@
+import Nav from "../components/nav/Nav";
+
+const ContractCode = () => {
+  return (
+    <Nav>
+      <div>
+        <p>Contract code on the way!</p>
+      </div>
+    </Nav>
+  );
+};
+export default ContractCode;
