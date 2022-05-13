a12e5f0932dc08346a159acf48be1fe837a5698b
Josh Loesch <jloesch30@gmail.com>
2022-05-13 14:21:38 -0500

    instruction changes

--- client/components/admin/CreateUserPanel.js
+++ client/components/admin/CreateUserPanel.js
@@ -72,11 +72,10 @@ const CreateUserPanel = () => {
           Please add your users below with their line items
         </h1>
         <p className="text-center mx-10 mb-3 text-xs md:text-base font-sans italic">
-          Each user is allowed to have one profile to interact with demo. Once
-          created, you will see the profile appear on the home page and can
-          begin submit &quotskills&quot to be verified. If you would like to
-          create a new profile, please use the delete button on your created
-          profile.
+          Each user is allowed to have one profile to interact with the demo.
+          Once created, you will see the profile appear on the home page and can
+          begin submit <q>skills</q> to be verified. If you would like to create
+          a new profile, please use the delete button on your created profile.
         </p>
         <form
           onSubmit={createNewUser}
